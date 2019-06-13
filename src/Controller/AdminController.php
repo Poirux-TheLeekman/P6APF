54cdcf800fc00c974581e8822a96a4459f5ecdd5
Poirux-TheLeekman <poirux69@gmail.com>
2019-06-13 12:48:53 +0200

    add user interface with registration,login,logout functions

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+use App\Entity\User;
+use App\Form\UserType;
 
 class AdminController extends AbstractController
 {
@@ -16,4 +23,65 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+    
+    
+/**
+ * ***************
+ * USER MANAGEMENT
+ * ***************
+ */
+    
+/**
+ * @Route("/user/new", name="user-new")
+ * @Route("/user/{}", name="user-edit")
+ */
+    public function User(User $user=null, Request $request, ObjectManager $om,UserPasswordEncoderInterface $encoder)
+    {
+        // create a new actor entity
+        if (!$user) {
+            $user= new User();
+            
+        }
+        //  create FormBuilder from form factory service
+        $formUser = $this->createForm(UserType::class, $user);
+        $formUser->handleRequest($request);
+        
+        if ($formUser->isSubmitted() && $formUser->isValid()) {
+            
+            $pwd=$encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($pwd);
+            $om->persist($user);
+            $om->flush();
+            
+            return $this->redirectToRoute('login');
+            
+        }else {
+            return $this->render('admin/registration.html.twig',  [
+                'form' => $formUser->createView()
+            ]);
+        }
+        
+    }
+    
+    /**
+     *
+     * @Route ("/login", name="login")
+     */
+    public function Login (){
+        
+        
+        return $this->render ('admin/login.html.twig');
+    }
+    
+    /**
+     *
+     * @Route ("/deconnexion", name="logout")
+     */
+    public function Logout (){
+        
+        
+    }
+    
+    
+    
 }
